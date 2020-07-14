6cb0911ecde48ebaffdafbdf7885fa2671c0bc87
Michele <michele@specsyst.com.br>
2020-07-14 15:02:00 -0300

    create src/products/templates/products/list.html and insert template name in src/products/views.py in ProductListView

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
-
 from django.views.generic import ListView
 from django.shortcuts import render
 
 from .models import Product
 
+#Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
+    template_name = "products/list.html"
